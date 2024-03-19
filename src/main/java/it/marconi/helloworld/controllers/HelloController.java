aa7bd642f8bd85fc8feae0902ad5384f29d01cec
francescoTilotta <francesco.tilotta@marconirovereto.it>
2024-03-19 09:36:07 +0100

    Controller created

---
+++ src/main/java/it/marconi/helloworld/controllers/HelloController.java
@@ -0,0 +1,12 @@
+package it.marconi.helloworld.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// stereotype annotation
+@Controller
+@RequestMapping("/")
+public class HelloController {
+    
+    // CRUD (Create Read Update Delete) -> HTTP (POST, GET, PUT, DELETE)
+}
